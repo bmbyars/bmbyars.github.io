c1a3ca8af33d1d3c5bb37e94ea29cb773f04fe87
bmbyars <bmbyars@gmail.com>
2022-04-22 11:50:11 -0500

    Update HelloWorld.js

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,31 +1,18 @@
 (function () {
 
+  // Add-in code goes here
+
   // BBSkyAddinClient is global here.
   var client = new BBSkyAddinClient.AddinClient({
     callbacks: {
       init: function(args) {
 
-        $('#environmentId').text(args.envId);
-        $('#contextRecordId').text(args.context.recordId);
+        // perform any initialization needed for the add-in
 
-        // wire up a click handler for the button
-        $("#getUserIdentityToken").click(getUserIdentityToken);
-
-        // inform the host page that the add-in is ready to be shown
+        // when the tile is ready to be shown, inform the host page
         args.ready({ showUI: true, title: 'My Custom Tile' });
       }
     }
   });
-
-  function getUserIdentityToken() {
-    $("#userIdentityToken").hide();
-    $("#userIdentityTokenValue").text("");
-
-    client.getUserIdentityToken().then(function(token) {
-      $("#userIdentityTokenValue").text(token);
-      $("#userIdentityToken").show();
-    });
-
-  }
 
 }());
